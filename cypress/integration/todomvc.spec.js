f52350e865a6f1b7af3cdb85a20bec4596cc879c
Gaurav Singh <gaurav.singh@go-jek.com>
2020-05-31 08:37:03 +0530

    Added watchForFileChanges as false and 2 basic tests for cypress

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,12 @@
 /// <reference types="cypress"/>
 
-it('should navigate to ToDoMVC app', () => {
+it('should be able to wait for specified delay', function() {
+    cy.visit("http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000")
+    cy.get('.new-todo', {timeout: 6000})
+})
+
+it('should be able to add a new todo to the list', () => {
     cy.visit("http://todomvc-app-for-testing.surge.sh/")
+    cy.get('.new-todo').type("Do chores{enter}")
+    cy.get('.toggle').click()
 })
